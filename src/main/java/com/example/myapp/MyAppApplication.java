bb9d267bbcea8043172b82488271a6bf0ec39329
shreyaadithya <shreyaadithya28@gmail.com>
2025-09-26 14:48:25 +0530

    Update MyAppApplication.java

--- src/main/java/com/example/myapp/MyAppApplication.java
+++ src/main/java/com/example/myapp/MyAppApplication.java
@@ -1,19 +1,29 @@
+package com.example.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
 public class MyAppApplication extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(MyAppApplication.class);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(MyAppApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MyAppApplication.class);
+    }
+
     @RestController
-    public static class HelloController {
+    class HelloController {
         @GetMapping("/")
         public String hello() {
-            return "Hello from CI/CD Pipeline on Tomcat!";
+            return "Hello, CI/CD Pipeline is working!";
         }
     }
 }
